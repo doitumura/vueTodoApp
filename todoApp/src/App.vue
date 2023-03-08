596df99dae5e8e036cfbee835917a6035c5eb1cb
doitumura <1993nakayama@gmail.com>
2023-03-08 13:44:10 +0900

    不要なコンポーネントを削除し、Todo一覧機能とTodo追加機能を作成

--- todoApp/src/App.vue
+++ todoApp/src/App.vue
@@ -1,47 +1,31 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+
+let id = 0
+
+const newTodo = ref("");
+const todos = ref([]);
+
+const addTodo = () => {
+  todos.value.push({id: id++, text: newTodo.value})
+  newTodo.value = ''
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<div>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo">
+    <button>Add Todo</button>
+  </form>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <p>{{todo.text}}</p>
+    </li>
+  </ul>
+</div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
