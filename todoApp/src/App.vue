e5c19767a76f0ab359a9f56dbc2e8cc566682f6c
doitumura <1993nakayama@gmail.com>
2023-03-09 11:07:30 +0900

    cssを調整

--- todoApp/src/App.vue
+++ todoApp/src/App.vue
@@ -28,26 +28,69 @@ const updateTodo = (todo) => {
 
 <template>
 <div>
-  <form @submit.prevent="addTodo">
-    <input v-model="newTodo">
-    <button>Add Todo</button>
-  </form>
+  <h1 class="heading">タスク管理アプリ</h1>
 
-  <ul>
-    <li v-for="todo in todos" :key="todo.id">
-      <form v-if="todo.edit" @submit.prevent="updateTodo(todo)">
-        <input v-model="todo.text">
-        <button>更新</button>
-      </form>
-      <div v-else>
-        <span>{{todo.text}}</span>
-        <button @click="editTodo(todo)">編集</button>
-        <button @click="removeTodo(todo)">削除</button>
-      </div>
-    </li>
-  </ul>
+  <div class="todo-input">
+    <h2>タスクを追加する</h2>
+    <form @submit.prevent="addTodo">
+      <input v-model="newTodo">
+      <button>追加</button>
+    </form>
+  </div>
+
+  <div class="todo-list">
+    <h2 v-if="todos.length > 0" class="list-heading">タスク一覧</h2>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <form v-if="todo.edit" @submit.prevent="updateTodo(todo)">
+          <input v-model="todo.text">
+          <button>更新</button>
+        </form>
+        <div v-else>
+          <span>{{todo.text}}</span>
+          <button @click="editTodo(todo)">編集</button>
+          <button @click="removeTodo(todo)">削除</button>
+        </div>
+      </li>
+    </ul>
+  </div>
 </div>
 </template>
 
 <style scoped>
+.heading {
+  font-size: 70px;
+}
+
+.todo-input {
+  margin-top: 20px;
+}
+
+form {
+  display: flex;
+  justify-content: left;
+}
+
+input {
+  height: 20px;
+}
+
+button {
+  height: 20px;
+  line-height: 12px;
+  font-size: 12px;
+}
+
+.todo-list {
+  margin-top: 30px;
+}
+
+ul {
+  padding-left: 15px;
+}
+
+span {
+  margin-right: 5px;
+  height: 20px;
+}
 </style>
